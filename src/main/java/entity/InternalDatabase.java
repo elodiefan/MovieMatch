6f86a7b3c9e9a0327a4c47e57ac5b8c6c1d6108d
Yidan Xu <yidanxu@YidandeMacBook-Air.local>
2026-07-28 16:51:31 -0400

    Add local database structure and TMDB generator setup

---
+++ src/main/java/entity/InternalDatabase.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalMovieDatabase {
+
+    private final List<Movie> movies;
+
+    public LocalMovieDatabase() {
+        movies = new ArrayList<>();
+
+
+    }
+}
