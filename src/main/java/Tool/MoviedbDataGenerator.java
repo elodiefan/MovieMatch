cbc1e2b7df2c99a961c488a61fa72f425934f74f
Yidan Xu <yidanxu@YidandeMacBook-Air.local>
2026-07-28 17:42:40 -0400

    Implement media feature

---
+++ src/main/java/Tool/MoviedbDataGenerator.java
@@ -0,0 +1,15 @@
+package Tool;
+
+/**
+ * Generating local movies and TV shows from TMDB.
+ */
+public class MoviedbDataGenerator {
+    /**
+     * Generating lacal JSON files containing 200 medias.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
