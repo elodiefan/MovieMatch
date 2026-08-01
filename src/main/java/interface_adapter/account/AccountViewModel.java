1d8d79a1c369066929241e22344da87a6ad1cf83
ckiersten <kiersten.chou@gmail.com>
2026-08-01 00:22:07 -0400

    made names of views and view models consistent so they would actually switch

--- src/main/java/interface_adapter/account/AccountViewModel.java
+++ src/main/java/interface_adapter/account/AccountViewModel.java
@@ -7,6 +7,7 @@ import interface_adapter.ViewModel;
  */
 public class AccountViewModel extends ViewModel<AccountState> {
 
+    public static final String VIEW_NAME = "account";
     public static final String TITLE_LABEL = "My Account";
     public static final String UPGRADE_TO_PREMIUM_BUTTON = "Upgrade to Premium";
     public static final String CUSTOMIZE_BUTTON = "Customize Profile";
