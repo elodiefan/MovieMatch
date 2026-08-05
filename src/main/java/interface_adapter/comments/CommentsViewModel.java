bedaab732d1ec7c1262803bf1a7012d434c955be
Elodie Fan <elodie.fan25@gmail.com>
2026-08-05 09:00:41 -0400

    added comments view model

--- src/main/java/interface_adapter/comments/CommentsViewModel.java
+++ src/main/java/interface_adapter/comments/CommentsViewModel.java
@@ -1,8 +1,23 @@
 package interface_adapter.comments;
 
+import interface_adapter.ViewModel;
+
 /**
  * View model for review comments.
  */
-public class CommentsViewModel {
+public class CommentsViewModel extends ViewModel<CommentsState> {
 
+    public static final String VIEW_NAME = "comments";
+    public static final String TITLE_LABEL = "Comments";
+    public static final String EMPTY_COMMENTS_MESSAGE = "No comments yet.";
+    public static final String WRITE_COMMENT_BUTTON_LABEL = "Write Comment";
+    public static final String REPLY_BUTTON_LABEL = "Reply";
+    public static final String DELETE_BUTTON_LABEL = "Delete";
+    public static final String LIKE_BUTTON_LABEL = "Like";
+    public static final String UNLIKE_BUTTON_LABEL = "Unlike";
+
+    public CommentsViewModel() {
+        super(VIEW_NAME);
+        setState(new CommentsState());
+    }
 }
