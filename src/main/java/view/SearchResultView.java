713dad1b265eaff9a34a395a0c307caa765b84c2
Yidan Xu <yidanxu@YidandeMacBook-Air.local>
2026-08-05 17:04:01 -0400

    Implement media detail use case

--- src/main/java/view/SearchResultView.java
+++ src/main/java/view/SearchResultView.java
@@ -1,7 +1,6 @@
 package view;
 
 import java.awt.Component;
-import java.awt.FlowLayout;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -10,6 +9,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import entity.Media;
+import interface_adapter.media_detail.MediaDetailController;
 import interface_adapter.search_result.SearchResultState;
 import interface_adapter.search_result.SearchResultViewModel;
 
@@ -20,6 +21,7 @@ public class SearchResultView extends JPanel implements PropertyChangeListener {
 
     private final String viewName = SearchResultViewModel.VIEW_NAME;
 
+    private MediaDetailController mediaDetailController;
     private final SearchResultViewModel searchResultViewModel;
 
     private final JPanel resultsPanel;
@@ -35,13 +37,20 @@ public class SearchResultView extends JPanel implements PropertyChangeListener {
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         resultsPanel = new JPanel();
-        resultsPanel.setLayout(new FlowLayout());
+        resultsPanel.setLayout(
+                new BoxLayout(resultsPanel, BoxLayout.Y_AXIS)
+        );
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
         this.add(resultsPanel);
         this.add(backButton);
+    }
+
+    public void setMediaDetailController(
+            MediaDetailController mediaDetailController) {
+        this.mediaDetailController = mediaDetailController;
     }
 
     @Override
@@ -54,18 +63,36 @@ public class SearchResultView extends JPanel implements PropertyChangeListener {
 
         if (state.getResults() != null) {
 
-            state.getResults().forEach(media -> {
-                resultsPanel.add(
-                        new JLabel(
-                                media.getTitle() + " (" + media.getReleaseYear() + ")"
-                        )
-                );
-            });
-
+            state.getResults().forEach(this::addMediaResult);
         }
 
         this.revalidate();
         this.repaint();
+    }
+
+    private void addMediaResult(Media media) {
+
+        final JPanel mediaPanel = new JPanel();
+
+        final JLabel mediaLabel =
+                new JLabel(
+                        media.getTitle()
+                                + " ("
+                                + media.getReleaseYear()
+                                + ")"
+                );
+
+        final JButton detailButton =
+                new JButton("Detail");
+
+        detailButton.addActionListener(
+                event -> mediaDetailController.execute(media)
+        );
+
+        mediaPanel.add(mediaLabel);
+        mediaPanel.add(detailButton);
+
+        resultsPanel.add(mediaPanel);
     }
 
     public String getViewName() {
