d0061b4604bd6d7e794fd3390ce1df56a251bccf
ckiersten <kiersten.chou@gmail.com>
2026-08-04 22:16:41 -0400

    worked on access message chat interactor

--- src/main/java/use_case/access_message_chat/AccessMessageChatInteractor.java
+++ src/main/java/use_case/access_message_chat/AccessMessageChatInteractor.java
@@ -19,12 +19,11 @@ public class AccessMessageChatInteractor implements AccessMessageChatInputBounda
     public void execute(AccessMessageChatInputData accessMessageChatInputData) {
         final String otherUsername = accessMessageChatInputData.getOtherUsername();
         if (userDataAccessObject.canMessage(otherUsername)) {
-            final AccessMessageChatOutputData accessMessageChatOutputData = new AccessMessageChatOutputData(false, false);
-            userPresenter.prepareFailView(accessMessageChatOutputData);
+            userPresenter.prepareAccessMessageChatFailView("Cannot message this user.");
         }
         else {
             final AccessMessageChatOutputData accessMessageChatOutputData = new AccessMessageChatOutputData(true, false);
-            userPresenter.prepareSuccessView(accessMessageChatOutputData);
+            userPresenter.prepareAccessMessageChatSuccessView(accessMessageChatOutputData);
         }
     }
 }
