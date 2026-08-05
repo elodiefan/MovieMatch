29dbe3122b5ed684199c78edda5d5e30dc780af8
Elodie Fan <elodie.fan25@gmail.com>
2026-08-05 08:36:24 -0400

    added user reviews view model

--- src/main/java/interface_adapter/user_reviews/UserReviewsViewModel.java
+++ src/main/java/interface_adapter/user_reviews/UserReviewsViewModel.java
@@ -1,8 +1,23 @@
 package interface_adapter.user_reviews;
 
+import interface_adapter.ViewModel;
+
 /**
  * View model for the user reviews view.
  */
-public class UserReviewsViewModel {
+public class UserReviewsViewModel extends ViewModel<UserReviewsState> {
 
+    public static final String VIEW_NAME = "user reviews";
+    public static final String TITLE_LABEL = "Reviews";
+    public static final String EMPTY_REVIEWS_MESSAGE = "No reviews yet.";
+    public static final String EDIT_BUTTON_LABEL = "Edit";
+    public static final String DELETE_BUTTON_LABEL = "Delete";
+    public static final String LIKE_BUTTON_LABEL = "Like";
+    public static final String UNLIKE_BUTTON_LABEL = "Unlike";
+    public static final String BACK_BUTTON_LABEL = "Back";
+
+    public UserReviewsViewModel() {
+        super(VIEW_NAME);
+        setState(new UserReviewsState());
+    }
 }
