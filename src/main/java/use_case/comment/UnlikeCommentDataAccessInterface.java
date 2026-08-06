34fca61bbc03a05996788ee8273df65c9b0b5acd
Elodie Fan <elodie.fan25@gmail.com>
2026-08-06 08:38:02 -0400

    added unlike comment data access interface

--- src/main/java/use_case/comment/UnlikeCommentDataAccessInterface.java
+++ src/main/java/use_case/comment/UnlikeCommentDataAccessInterface.java
@@ -5,4 +5,11 @@ package use_case.comment;
  */
 public interface UnlikeCommentDataAccessInterface {
 
+    /**
+     * Removes a user's like from one comment.
+     * @param commentId the id of the comment to unlike
+     * @param username the username of the user unliking the comment
+     * @return true if the comment was found and unliked
+     */
+    boolean unlikeComment(String commentId, String username);
 }
